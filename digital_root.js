8fd4130c02b7f35cad53ef06487113068433d2d3
admin <admin@admins-MacBook-Pro.local>
2022-05-15 22:12:38 +0100

    digital root function that call itself recursively to reduce a number more than one to one digit

--- digital_root.js
+++ digital_root.js
@@ -1,17 +1,13 @@
-// Digital root is the recursive sum of all the digits/binary in a number.
-// Given n, take the sum of the digits/binary of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. 
 // The input will be a non-negative integer.
 
-function digital_binary_root(n) {
-    // ...
-    let sum=0;
-    let digitsOfN = (n>>> 0).toString(2);
-   let arrayDigit = digitsOfN.split('');
-    if(arrayDigit.length > 1){
-     sum = arrayDigit.reduce((acc,current) => acc+ Number(current),0)
-    }
 
-    return sum;
-  }
-  
-  console.log(digital_root(30));
+
+  function digital_root(number) {
+    // ...
+    if(number < 10)return number;
+         return  digital_root(number.toString().split('').reduce((acc,current) => acc+Number(current),0))
+}
+    
+  console.log(digital_root(1233399998888));
